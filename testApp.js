5fed03edd4925c5fa247221f2ef5be3c9ac2fff8
이명진 <audwls172@gmail.com>
2022-11-04 23:10:27 +0900

    class로 import하기

--- testApp.js
+++ testApp.js
@@ -1,1 +1,11 @@
-import fillCalendar from './calendar.js';
+import { calendar } from './testCalendar.js';
+
+class App {
+    constructor() {
+        fillCalender(selectMonth.value);
+    }
+}
+
+window.onload = () => {
+    new App();
+};
