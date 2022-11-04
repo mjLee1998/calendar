6c8fe3418442e7c9e97c6bc751752f5956222651
이명진 <audwls172@gmail.com>
2022-11-05 00:08:58 +0900

    .

--- getClientIp.js
+++ getClientIp.js
@@ -1,4 +1,4 @@
-export default async function getClientIP() {
+export async function getClientIP() {
     try {
         const response = await axios.get('https://api.ipify.org?format=json');
         console.log(response.data.ip);
