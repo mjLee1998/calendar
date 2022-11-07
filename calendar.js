64da331ae692fe5f45c13785838df4eb351d065f
이명진 <audwls172@gmail.com>
2022-11-07 18:26:17 +0900

    .

--- calendar.js
+++ calendar.js
@@ -5,7 +5,6 @@ export class calendar {
         const month = today.getMonth() + 1;
         const date = today.getDate();
         console.log(year, month, date);
-        console.log(today);
         // 해당 달의 일 수 구하기
         var lastDay;
 
@@ -21,11 +20,11 @@ export class calendar {
         // 현재 해당하는 달로 이동하기
         const nowMonth = document.querySelector('select :nth-child(' + month + ')');
         nowMonth.setAttribute('selected', 'selected');
+
         // 달력 채우기
         // 달을 바꿀 때마다 자동으로 채워주기 위해서 select에 onchange를 걸어서 value를 여기로 보내서 처리
         function fillCalendar(mV) {
             const firstDay = new Date(year, mV - 1, 1);
-            // console.log(firstDay);
             var w = firstDay.getDay();
             const firstDayWeek = WeekDay[firstDay.getDay()];
             var x = w;
@@ -69,7 +68,7 @@ export class calendar {
 
         // 다른 달을 선택하면 바꿔주기
         const selectedMonth = document.querySelector('#selectMonth');
-        selectedMonth.onchange = function () {
+        selectedMonth.onchange = () => {
             // 지금 달력 다 지우기
             var everyDay = document.querySelectorAll('td');
             // console.log(everyDay);
@@ -79,6 +78,52 @@ export class calendar {
             }
             //다시 채우기
             fillCalendar(selectedMonth.value);
+        };
+        const prevClick = document.querySelector('#previousMonth');
+        prevClick.onclick = () => {
+            var everyDay = document.querySelectorAll('td');
+            var i;
+            for (i = 0; i < 42; i++) {
+                everyDay[i].innerHTML = '';
+            }
+            if (selectedMonth.value == 1) {
+                fillCalendar(12);
+            } else {
+                fillCalendar(selectedMonth.value - 1);
+            }
+            var prevMonth;
+            if (selectedMonth.value == 1) {
+                prevMonth = document.querySelector('#M12');
+            } else {
+                prevMonth = document.querySelector('#M' + (selectedMonth.value - 1));
+            }
+            console.log(prevMonth);
+            prevMonth.setAttribute('selected', 'selected');
+            // const months = document.querySelectorAll('option');
+            // 지금이 11월인데 처음에 selected가 부여되어 있어서 새로 selected가 부여되지 않음
+            // selected 해제하는 법 배우기
+        };
+        const nextClick = document.querySelector('#nextMonth');
+        nextClick.onclick = () => {
+            var everyDay = document.querySelectorAll('td');
+            var i;
+            for (i = 0; i < 42; i++) {
+                everyDay[i].innerHTML = '';
+            }
+            if (selectedMonth.value == 12) {
+                fillCalendar(1);
+            } else {
+                fillCalendar(selectedMonth.value + 1);
+            }
+            var nextMonth;
+            if (selectedMonth.value == 12) {
+                nextMonth = document.querySelector('#M1');
+            } else {
+                nextMonth = document.querySelector('#M' + (selectedMonth.value + 1));
+            }
+            console.log(nextMonth);
+            nextMonth.setAttribute('selected', 'selected');
+            // nextMonth.setAttribute('selected', 'selected');
         };
     }
 }
