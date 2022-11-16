5956b64bbf43ba712a3db9815c5e01f253250cbc
이명진 <audwls172@gmail.com>
2022-11-16 20:40:45 +0900

    .

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import { calendar } from './calendar.js';
 import { getClientIP } from './getClientIp.js';
-=======
-import calendar from './calendar.js';
-import getClientIP from './getClientIp.js';
->>>>>>> b4514e646ea1c49a6c4e4ba3ba198cb172512016
 import vote from './vote.js';
 class App {
   constructor() {
