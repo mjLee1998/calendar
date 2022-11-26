a299a93aa701acef73f10e73f280d09ad7516392
이명진 <audwls172@gmail.com>
2022-11-26 11:48:02 +0900

    .

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import calendar from './calendar.js';
 import getClientIP from './getClientIp.js';
 import vote from './vote.js';
-// import axios from './node_modules/axios/lib/axios.js';
 class App {
   constructor() {
     new calendar();
