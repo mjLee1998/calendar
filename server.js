839ffef02ed44531a547443aea3f5deaa1ca1b30
이명진 <audwls172@gmail.com>
2022-12-09 16:11:12 +0900

    .

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var app = require('express')();
 var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')(http, { cors: { origin: '*' } });
 
 var port = 3000;
 http.listen(port, () => {
