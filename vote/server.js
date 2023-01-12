cabb6a4230b3779629636f9d9f48538f7825075f
이명진 <audwls172@gmail.com>
2023-01-12 19:48:04 +0900

    .

--- vote/server.js
+++ vote/server.js
@@ -3,42 +3,21 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-// 웹 서버 구동
-server.listen(3000, () => {
-  console.log('Server started on port 3000');
+let results = {
+  option1: 0,
+  option2: 0,
+  option3: 0,
+};
+
+io.on('connection', (socket) => {
+  socket.emit('results', results);
+
+  socket.on('vote', (vote) => {
+    results[vote] += 1;
+    io.emit('results', results);
+  });
 });
 
-// 접속한 클라이언트를 저장할 배열
-const clients = [];
-
-// socket.io 설정
-io.on('connection', (socket) => {
-  console.log('Client connected:', socket.id);
-
-  // 클라이언트 접속 시
-  socket.on('vote', (data) => {
-    // 접속한 클라이언트 배열에 추가
-    clients.push({
-      id: socket.id,
-      vote: data,
-    });
-
-    // 접속한 모든 클라이언트에게 투표 결과 전송
-    io.emit('vote', clients);
-  });
-
-  // 클라이언트 접속 해제 시
-  socket.on('disconnect', () => {
-    console.log('Client disconnected:', socket.id);
-
-    // 접속 해제한 클라이언트를 배열에서 제거
-    clients.forEach((client, index) => {
-      if (client.id === socket.id) {
-        clients.splice(index, 1);
-      }
-    });
-
-    // 접속한 모든 클라이언트에게 투표 결과 전송
-    io.emit('vote', clients);
-  });
+server.listen(3000, () => {
+  console.log('Server started on http://localhost:3000');
 });
--- vote/vote.js
+++ vote/vote.js
@@ -1,19 +1,19 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const vote = new FormData(form).get('vote');
+  socket.emit('vote', vote);
+});
+
 const socket = io();
 
-// 투표 이벤트
-function vote(vote) {
-  socket.emit('vote', vote);
-}
-
-// 투표 결과 수신 이벤트
-socket.on('vote', (data) => {
-  // 투표 결과를 HTML 문서에 표시
-  const voteList = document.getElementById('vote-list');
-  voteList.innerHTML = ''; // 기존 데이터 삭제
-
-  data.forEach((client) => {
-    const item = document.createElement('li');
-    item.innerHTML = `${client.vote}: ${client.vote}표`;
-    voteList.appendChild(item);
-  });
+socket.on('results', (data) => {
+  const results = document.querySelector('#results');
+  results.innerHTML = '';
+  for (const [option, count] of Object.entries(data)) {
+    const p = document.createElement('p');
+    p.innerHTML = `${option}: ${count}`;
+    results.appendChild(p);
+  }
 });
